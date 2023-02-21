6a7be7648d18a0920aa0404880dc84a8db49bc69
19021369 <gamehay.net12321@gmail.com>
2023-02-21 23:55:08 +0700

    searchresults more

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 
 function Header() {
     const [navIsShown, setnavIsShown] = useState(false);
+
     const toggleNavIsShown = () => {
         setnavIsShown((navIsShown) => !navIsShown);
     };
-    const [searchBarIsShown, setSearchBarIsShown] = useState(false);
+    const [searchArea, setSearchArea] = useState('')
     return (
         <nav className="flex justify-between items-center h-15 px-4 z-10 w-full bg-gray-300">
             <a href="/">
-                <h1 className='w-80'>FIODE</h1>
+                <h1 className="w-80">FIODE</h1>
             </a>
 
             <ul className="hidden md:flex font-semibold ">
@@ -31,8 +32,9 @@ function Header() {
             </ul>
 
             <div className="hidden md:flex">
-                    <form className="max-w-sm px-4">
-                        <div className="relative">
+                <form className="max-w-sm px-4">
+                    <div className="relative">
+                        <a href='/searchresults'>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
@@ -47,13 +49,18 @@ function Header() {
                                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                                 />
                             </svg>
-                            <input
-                                type="text"
-                                placeholder="Search"
-                                className="w-full py-2 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
-                            />
-                        </div>
-                    </form>
+                        </a>
+
+                        <input
+                            value={searchArea}
+                            onChange={e => setSearchArea(e.target.value)}
+                            type="text"
+                            placeholder="Search"
+                            className="w-full py-2 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
+                        />
+                    </div>
+                </form>
+
                 <a href="/login" className="flex items-center">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
--- src/components/Layout/components/SearchResults/SearchResultsImg.js
+++ src/components/Layout/components/SearchResults/SearchResultsImg.js
@@ -4,14 +4,37 @@ import searchResults from '~/assets/searchresult.jpg';
 function SearchResultsImg() {
     return (
         <div>
-            <div
-                className="max mx-auto pt-16 px-4 text-center h-72"
-                style={{ 
-                  backgroundImage: `url(${searchResults})`,
-                  backgroundSize: "cover"
-                  }}
-            >
-                Huy an cut
+            <div className="relative">
+                <div
+                    className="mx-auto px-4 filter brightness-50"
+                    style={{
+                        backgroundImage: `url(${searchResults})`,
+                        backgroundSize: 'cover',
+                        height: '700px',
+                    }}
+                ></div>
+                <h1 className="translate-y-1/2 pl-4 text-6xl text-white absolute inset-y-1/2">
+                    Search results for:
+                </h1>
+            </div>
+
+            <div className="flex justify-center pt-16">
+                <div className="flex border border-purple-200 rounded-lg">
+                    <input
+                        type="text"
+                        className="w-96 block w-full px-4 py-2 text-purple-700 bg-white border rounded-lg focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        placeholder="Search..."
+                    />
+                    <button className="px-4 text-white bg-purple-600 border-l rounded-lg ">
+                        Search
+                    </button>
+                </div>
+            </div>
+            <div className='pt-16 space-x-16 max-w-[1240px] mx-auto pt-16 px-4 text-center'>
+                <button className='w-60 rounded-full font-bold'>Destinations (0)</button>
+                <button className='w-60 rounded-full font-bold'>Regions (0)</button>
+                <button className='w-60 rounded-full font-bold'>Festivals & Events (0)</button>
+                <button className='w-60 rounded-full font-bold'>Blogs (0)</button>
             </div>
         </div>
     );
